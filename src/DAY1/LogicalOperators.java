b624c51450d3a94b0fb2855c581a4c22b55558dc
Kishore0921 <kishorenbkk@gmail.com>
2026-08-17 14:54:56 +0530

    Basics

---
+++ src/DAY1/LogicalOperators.java
@@ -0,0 +1,14 @@
+package DAY1;
+
+public class LogicalOperators {
+
+    public static void main(String[] args)
+    {
+        int age=22;
+
+        boolean student=true;
+
+        System.out.println(age>=20 && student);
+        System.out.println(age != 11 && !student);
+    }
+}
