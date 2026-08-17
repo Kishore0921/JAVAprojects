18b006a5da5e5845877cdae66e016c9c51bdd3f9
Kishore0921 <kishorenbkk@gmail.com>
2026-08-17 13:00:17 +0530

    Basics

---
+++ src/DAY1/ArthimaticOperators.java
@@ -0,0 +1,13 @@
+package DAY1;
+
+public class ArthimaticOperators {
+    public static void main(String[] args){
+        int a=20;
+        int b=30;
+
+        System.out.println("Addition : "+(a+b));
+        System.out.println("Subtration : "+(a-b));
+        System.out.println("Multiplication : "+(a*b));
+        System.out.println("Division : "+(a/b));
+    }
+}
