7e6cd50a30029fb8b604c0a205db2803b0303c83
Kishore0921 <kishorenbkk@gmail.com>
2026-08-18 09:52:01 +0530

    Basics

---
+++ src/DAY2/OperatorPrecedence.java
@@ -0,0 +1,11 @@
+package DAY2;
+
+public class OperatorPrecedence {
+    public static void main(String[] args){
+        int res = 10 + 5 * 2;
+        System.out.println(res);
+
+        int val = (10 + 5) * 2;
+        System.out.println(val);
+    }
+}
