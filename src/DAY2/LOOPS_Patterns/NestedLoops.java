d35c7d02f045b6eb21ce49aa071c0a992084f442
Kishore0921 <kishorenbkk@gmail.com>
2026-08-18 10:53:27 +0530

    Day 2 Fundamentals

---
+++ src/DAY2/LOOPS_Patterns/NestedLoops.java
@@ -0,0 +1,11 @@
+package DAY2.LOOPS_Patterns;
+
+public class NestedLoops {
+    public static void main(String[] args){
+        for(int i=1;i<=3;i++){
+            for(int j=1;j<=3;j++) {
+                System.out.println(i + " time(s) " + j);
+            }
+        }
+    }
+}
---
+++ src/DAY2/NestedConditions.java
@@ -0,0 +1,14 @@
+package DAY2;
+
+public class NestedConditions {
+    public static void main(String[] args){
+        int marks=85;
+        if(marks >= 35)
+        {
+            if(marks <= 95)
+            {
+                System.out.println("Secured Distinction");
+            }
+        }
+    }
+}
