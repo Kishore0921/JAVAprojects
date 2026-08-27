c2d123773b8ba05833bd106652a505f2e1b66c22
Kishore0921 <kishorenbkk@gmail.com>
2026-08-27 10:10:47 +0530

    day-7 fundamentals

---
+++ src/DAY7/strtaray.java
@@ -0,0 +1,10 @@
+package DAY7;
+
+public class strtaray {
+    public static void main(String[] args){
+        String str = "Sapthagiri";
+        for(int i=0;i<str.length();i++){
+            System.out.println("Char of STR index is = "+str.charAt(i));
+        }
+    }
+}
