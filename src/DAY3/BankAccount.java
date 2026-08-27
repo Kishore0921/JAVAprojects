6d7573bb973c5889a825714111a136271a6764d7
Kishore0921 <kishorenbkk@gmail.com>
2026-08-27 09:41:07 +0530

    day-7 fundamentals

--- src/DAY3/BankAccount.java
+++
@@ -1,8 +0,0 @@
-package DAY3;
-import java.util.Scanner;
-public class BankAccount {
-    public static void main(String[] args){
-        BankAccount ba = new BankAccount();
-        System.out.println(ba);
-    }
-}
---
+++ src/DAY7/String1.java
@@ -0,0 +1,11 @@
+package DAY7;
+
+public class String1 {
+        public static void main(String[] args){
+            String str="hello";
+            System.out.println("my string"+str);
+            System.out.println("string length=="+str.length());
+            System.out.println("the first character"+str.charAt(0));
+            System.out.println("the second character"+str.charAt(1));
+        }
+}
