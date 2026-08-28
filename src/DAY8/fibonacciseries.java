47b90194aef8ed2b0a6df047442876846daaeb61
Kishore0921 <kishorenbkk@gmail.com>
2026-08-28 10:11:01 +0530

    day-8 fundamentals

---
+++ src/DAY8/fibonacciseries.java
@@ -0,0 +1,14 @@
+package DAY8;
+
+public class fibonacciseries {
+    static int fibonacci(int n){
+        if (n<=1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+    public static void main(String[] args){
+        int n = 6;
+        System.out.println("Fibonacci = "+fibonacci(n));
+    }
+}
