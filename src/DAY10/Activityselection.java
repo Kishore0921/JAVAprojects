8bd0c5971d877f0a63e484f553ab8952ebbd5616
Kishore0921 <kishorenbkk@gmail.com>
2026-09-01 11:53:06 +0530

    Day10

--- src/DAY10/Activityselection.java
+++ src/DAY10/Activityselection.java
@@ -1,34 +1,47 @@
 package DAY10;
 import java.util.*;
+import java.util.*;
 
-class Activity {
+class Activity{
     int start;
     int end;
 
     Activity(int start, int end){
-        this.start = start;
-        this.end = end;
+        this.start=start;
+        this.end=end;
     }
 }
-public class Activityselection {
+
+public class Activitys selection {
     public static void main(String[] args){
-        Activity[] activities = {
+
+        Activity[] activities= {
+
                 new Activity(1,3),
                 new Activity(2,4),
                 new Activity(3,5),
-                new Activity(5,7),
                 new Activity(6,8)
         };
+
         Arrays.sort(
                 activities,
-                Comparator.comparingInt(a ->a.end)
+                Comparator.comparing(a ->a.end)
         );
-        int lastEnd = 0;
-        for (Activity activity : activities){
-            System.out.println(
-                    activity.start + "-" + activity.end
-            );
-            lastEnd = activity.end;
+
+        int lastEnd=0;
+        for(Activity activity:activities){
+            if(activity.start>=lastEnd){
+
+                if(activity.start>=lastEnd){
+
+                    System.out.println(
+                            activity.start +"-"+
+                                    activity.end
+                    );
+
+                    lastEnd=activity.end;
+                }
+            }
         }
     }
 }
